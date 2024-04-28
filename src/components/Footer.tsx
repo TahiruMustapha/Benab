c8de42c318609e087e88f2ac71b5cc93d297ffd9
Mustapha tahiru <tahirumustapha13@gmail.com>
2024-04-28 18:00:26 +0000

    new changes again

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,11 @@ import { AiOutlineCopyright } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="w-full bg-[#F5F5F3]">
-      <Container className="max-w-container mx-auto border-t-[1px] group">
+      {/* <Container className="max-w-container mx-auto border-t-[1px] group">
         <p className="text-titleFont font-normal text-center flex md:items-center justify-center text-lightText duration-200 text-sm">
           <span className="text-md mr-[1px] mt-[2px] md:mt-0 text-center hidden md:inline-flex">
             <AiOutlineCopyright />
-          </span>
+          </span> 
           Copyright 2024 | Benab shopping | All Rights Reserved |
           <a href="https://portfolio-next-five-eosin.vercel.app/" target="_blank" >
             <span className="ml-1 font-medium group-hover:text-primeColor">
@@ -17,7 +17,7 @@ const Footer = () => {
             </span>
           </a>
         </p>
-      </Container>
+      </Container> */}
     </div>
   );
 };
